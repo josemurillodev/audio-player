2ee2d9ca4435c1af4014893d7994bf84e28cc212
Jose Murillo <joseluismurillorios@gmail.com>
2025-05-29 21:37:28 -0700

    Added plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
   base: '/audio-player/', // 👈 important!
+  plugins: [
+    visualizer({ template: 'treemap' }),
+  ],
 });
